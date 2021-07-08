b8dc58c6f704f420677a542267e6e74e90ad6386
Nate <flyingwithnate@pm.me>
2021-07-07 20:42:07 -0700

    added needsWater and main loop

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import tweepy
 import time
 import board
+from secrets import *
 
 from adafruit_seesaw.seesaw import Seesaw
 
+# global value for i2c board. Easy access to light sensor when ready.
 i2c_bus = board.I2C()
 
+# auth_user_tweepy & post_tweet written by Revekka K. from Twitchess
 def auth_user_tweepy():
     auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(token,token_secret)
@@ -22,6 +25,29 @@ def checkWater():
     touch = ss.moisture_read()
     return touch
 
-#def needsWater():
+# Runs through checkWater func to get back moisture value.
+# If value is over 700, then the Marvin is moisturized.
+# Anything below 700 but above 500 sends a message that it is running low.
+# Anything below 500 and the Marvin needs water.
+# Utilizes post_tweet() func to fire off tweet.
+def needsWater():
+    if checkWater() > 700:
+        post_tweet("Hail hydrate.")
+    elif checkWater() < 700 and checkWater() > 500:
+        post_tweet("I will require water soon.")
+    else:
+        post_tweet("Water me, please.")
 
 #def needsLight():
+    # will need to figure out how to connect light sensor via i2c later
+    # might need a new cable
+
+# main loop. runs every 60 minutes.
+# bad error message, but I'll figure out debugging later.
+try:
+    while True:
+       needsWater()
+       time.sleep(3600)
+
+except Exception:
+    print("This is a bad error message, but an error has occurred.")
